f3d6e9da65897df09aa5707c9baf8a48b004f761
WookjinJeong <40513208+WookjinJeong@users.noreply.github.com>
2018-06-23 15:31:47 +0900

    Add files via upload

---
+++ post.js
@@ -0,0 +1,43 @@
+const request = require('request');
+
+module.exports = (postAddress, subId, subName, serverId) => {
+    let options = {
+        uri: postAddress,
+        method: 'POST',
+        timeout: 20000,
+        json: {
+            subId: subId,
+            subName: subName,
+            server: serverId
+        }
+    };
+
+    start(options);
+};
+
+
+async function start(options) {
+    try {
+        for (let i = 1; i <= 3; i++) {
+            let res = await doPost(options);
+            if (res === 'OK') {
+                break;
+            }
+        }
+    } catch (error) {
+        //console.log(error);
+    }
+}
+
+function doPost(options) {
+    return new Promise((resolve, reject) => {
+        request(options, (error, response, body) => {
+            if (!error) {
+                resolve(body);
+                console.log("www")
+            } else {
+                reject(error);
+            }
+        });
+    });
+}
